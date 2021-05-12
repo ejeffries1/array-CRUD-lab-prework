170724267104928534d83655707fc3278acfac42
Evan Jeffries <ejeffries14@gmail.com>
2021-05-12 02:20:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  marvel = ["ironman", "black panther", "captain america", "spiderman"]
+  marvel << "hulk"
 end
 
 def add_element_to_start_of_array(array, element)
